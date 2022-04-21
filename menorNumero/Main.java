fa4ca3bc6bcab25529421aea82968acff1eec7ee
lyviamedeiroos <santosannslyvia@gmail.com>
2022-04-21 19:03:38 -0300

    Criando calculadora

--- menorNumero/Main.java
+++ menorNumero/Main.java
@@ -1,4 +1,4 @@
-package projetoJavaUnidade1.menorNumero;
+package Projeto.UserssantoIdeaProjectsUNIESP.POOsrcProjetoprojetoJavaUnidade1.menorNumero;
 
 /*
 Desenvolva um algoritmo capaz de encontrar o menor dentre 3 números inteiros quaisquer dados pelo teclado.
@@ -6,8 +6,30 @@ Desenvolva um algoritmo capaz de encontrar o menor dentre 3 números inteiros qu
 */
 
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        char operator;
+        int numero1, numero2, numero3;
+
+        Scanner input = new Scanner(System.in);
+        //para escolher o operador
+
+        System.out.println("Informe um operador: +, -, *, /");
+        operator = input.next().charAt(0);
+
+        // informe os numeros
+        System.out.println("Informe o primeiro número");
+        numero1 = input.nextInt();
+
+        System.out.println("Informe o segundo número");
+        numero2 = input.nextInt();
+
+        System.out.println("Informe o terceiro número");
+        numero3 = input.nextInt();
+
 
     }
 }
